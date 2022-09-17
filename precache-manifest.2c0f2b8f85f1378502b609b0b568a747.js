aaddb667cf35a0e4683c134cb4b4c5a4c6aa2a25
Temuge <93143245+Temugeee@users.noreply.github.com>
2022-09-17 16:22:29 +0800

    Updates

---
+++ precache-manifest.2c0f2b8f85f1378502b609b0b568a747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9102989dc7a3f5b71cb97448e1da962c",
+    "url": "/burger1/index.html"
+  },
+  {
+    "revision": "3015ff486bee246d5aa4",
+    "url": "/burger1/static/css/main.82400687.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger1/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger1/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3015ff486bee246d5aa4",
+    "url": "/burger1/static/js/main.0e5b9c9b.chunk.js"
+  },
+  {
+    "revision": "a2a21ccc7281daecfff6",
+    "url": "/burger1/static/js/runtime-main.d04bf0bc.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger1/static/media/burger-logo.b8503d26.png"
+  }
+]);
